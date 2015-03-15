82d13945d86f0887a0dbb5e1d30276df74a38a7c
Dana Peele <dana_peele@s5a.com>
2015-03-14 21:09:36 -0400

    Fixed init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,23 +2,23 @@ class dashing {
 
     package { 'install_dashing':
         ensure   => 'installed',
-        provider => 'gem',
+        provider => 'gem'
     }  
   
     exec { 'create_dashing_dashboard':
         command => 'dashing new hbc_status',
-        require => Package['install_dashing'],
+        require => Package['install_dashing']
     }
 
     exec { 'install_dashing_gems':
         command => 'bundle',
         cwd => '/home/vagrant/hbc_status',
-        require => Exec['create_dashing_dashboard'],
+        require => Exec['create_dashing_dashboard']
     }
 
     exec { 'start_dashing':
         command => 'dashing start',
-        require => Exec['install_dashing_gems'],
+        require => Exec['install_dashing_gems']
     }
 
 }
